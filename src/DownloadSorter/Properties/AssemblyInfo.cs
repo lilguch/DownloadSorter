d14904639585d961cb560d7a6417683bea6af25a
Dmitrij Alexandrov <mira199688@gmail.com>
2025-11-10 22:43:59 +0300

    Create AssemblyInfo.cs

---
+++ src/DownloadSorter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("DownloadsSorter")]
+[assembly: AssemblyDescription("Сортировщик файлов в папке загрузки")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dmitrij Alexandrow")]
+[assembly: AssemblyProduct("DownloadsSorter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("Dmitrij Alexandrow")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(true)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("25e08ac8-524c-415d-b1c0-7b27fe1e613e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
